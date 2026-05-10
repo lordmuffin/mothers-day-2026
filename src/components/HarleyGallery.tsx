e3a989896cb88b63dc34b7425f87e94425ae1c77
[dyad] <dorkmeisterx69@gmail.com>
2026-05-10 11:37:30 -0500

    Updated src/components/HarleyGallery.tsx

--- src/components/HarleyGallery.tsx
+++ src/components/HarleyGallery.tsx
@@ -6,11 +6,11 @@ const HarleyGallery = () => {
   // Using high-quality placeholders for now
   const photos = [
     "https://photos.fife.usercontent.google.com/pw/AP1GczPA4z-n9x63nUB5XioU2T_FqcL7ojZ61Ams39pm2rfUfbtvmlYiv00TrA=w1703-h1277-s-no-gm?authuser=0",
+    "https://photos.fife.usercontent.google.com/pw/AP1GczMIY41dbde4K2bF-i48c3_qobohD7myAxF3qiNCERGkx5PTU6IM_YNysg=w1703-h1277-s-no-gm?authuser=0",
     "",
     "",
     "",
-    "",
-    
+
   ];
 
   return (
